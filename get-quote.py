3a04461e79e59af679a61a1661f2a16463b131c3
psicoRoberto <betopr18@gmail.com>
2020-06-15 01:55:31 -0300

    Next steps

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,12 @@
+#Known issue:
+#f.write will keep writing the same line over and over again to the txt file.
+#The print command prints the first line in a different way than the others.
+
 import random
 def primary():
    # print("Keep it logically awesome. Please")
+
+   #this part adds quotes to the txt file
   f= open("quotes.txt", "a")
   f.write("Now the file has more content!\n")
   f.write("This test is working\n")
@@ -10,14 +16,15 @@ def primary():
   quotes = f.readlines()
   f.close()
 
+#A simple way to get multiple random quotes
   last= len(quotes) - 1
   rnd  = random.randint(0, last)
   rnd0 = random.randint(0, last)
   rnd1 = random.randint(0, last)
   rnd2 = random.randint(0, last)
 
-  print (quotes [rnd] end='')
-  print (quotes [rnd0] end='') 
+#Use of (end='') to remove the newline at the end
+  print (quotes [rnd], quotes [rnd0], quotes [rnd1], quotes [rnd2], end='')
 
 if __name__== "__main__":
   primary()
