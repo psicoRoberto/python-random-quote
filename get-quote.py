7c4800bdac772ed647912f43f9b2a4f4589e1496
psicoRoberto <betopr18@gmail.com>
2020-06-15 01:46:35 -0300

    Course is completed + 'next step' session

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,24 @@
 import random
 def primary():
    # print("Keep it logically awesome. Please")
+  f= open("quotes.txt", "a")
+  f.write("Now the file has more content!\n")
+  f.write("This test is working\n")
+  f.close()
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last= len(quotes) - 1
-  rnd = random.randint(0, last)
+  rnd  = random.randint(0, last)
+  rnd0 = random.randint(0, last)
+  rnd1 = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print (quotes [rnd] end='')
+  print (quotes [rnd0] end='') 
 
 if __name__== "__main__":
   primary()
+  
